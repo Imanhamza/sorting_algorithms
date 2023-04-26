f4dc2a8e6c378338a5d70664bdb0404291f7bb94
Iman Hamza <emyali697@gmail.com>
2023-04-25 19:33:30 -0700

    update sort.h

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void insertion_sort_list(listint_t **list);
 void nswap(listint_t **head, listint_t **node1, listint_t *node2);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
